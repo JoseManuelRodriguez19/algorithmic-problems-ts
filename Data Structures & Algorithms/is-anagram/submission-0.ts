a0ae55b6b74c7920fa621b264a51978092d88c4f
JoseManuelRodriguez19 <rodriguez.reyes.josemanuel15@gmail.com>
2026-07-16 17:18:26 -0600

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.ts
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -5,15 +5,16 @@ class Solution {
      * @return {boolean}
      */
     isAnagram(s: string, t: string): boolean {
-        if (s.length !== t.length) {
+        if(s.length !== t.length){
             return false;
         }
-
+      
         const count = new Array(26).fill(0);
-        for (let i = 0; i < s.length; i++) {
-            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-            count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+        for (let i = 0; i < s.length; i++){
+           count[s.charCodeAt(i)- 97] ++;
+           count[t.charCodeAt(i)-97] --;
         }
         return count.every((val) => val === 0);
     }
+
 }
